58acce01ca4a335bece5b8d5214b48cfcd9690e2
keny <64864411+kenybapin@users.noreply.github.com>
2023-12-10 14:41:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
 from flask import Flask, render_template, redirect, request, session
 import requests
 
 app = Flask(__name__)
 
+
 # Disabling cache,  note:  Flask didn't see the updates in JS and CSS files
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-APP_ID = '########################'
-APP_SECRET = '#############################'
+APP_ID = '######'
+APP_SECRET = '######'
 app.secret_key = APP_SECRET
 
 API_BASE = 'https://accounts.spotify.com'
@@ -21,7 +25,17 @@ url_top_tracks = 'https://api.spotify.com/v1/me/top/tracks?time_range=long_term&
 url_saved_tracks = 'https://api.spotify.com/v1/me/tracks?limit=5&offset=5'
 url_recently_played = 'https://api.spotify.com/v1/me/player/recently-played?limit=10&offset=5'
 url_current_user = 'https://api.spotify.com/v1/me'
+url_top_genres = 'https://api.spotify.com/v1/me/top/artists?time_range=long_term&limit=100&offset=0'
 
+# to do 
+url_variables = {
+    'url_top_artists': 'https://api.spotify.com/v1/me/top/artists?time_range=long_term&limit=5&offset=5',
+    'url_top_tracks': 'https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=5&offset=5',
+    'url_saved_tracks': 'https://api.spotify.com/v1/me/tracks?limit=5&offset=5',
+    'url_recently_played': 'https://api.spotify.com/v1/me/player/recently-played?limit=10&offset=5',
+    'url_current_user': 'https://api.spotify.com/v1/me',
+    'url_top_genres': 'https://api.spotify.com/v1/me/top/artists?time_range=long_term&limit=100&offset=0'
+}
 
 
 # AUTH Step 1: log in and Request for User Authorization
@@ -50,6 +64,21 @@ def api_callback():
     session["token"] = res_body.get("access_token")
     return redirect("home")
 
+## RETRIEVE DATA
+
+def get_genre_counts(data_top_genres):
+    genre_list = [genre['genres'] for genre in data_top_genres['items']]
+    genre_counts = {}
+
+    for artist_genres in genre_list:
+        for genre in artist_genres:
+            genre_counts[genre] = genre_counts.get(genre, 0) + 1
+    return genre_counts
+
+def get_top_n_genres(genre_counts, n=5):
+    sorted_genres = sorted(genre_counts.items(), key=lambda x: x[1], reverse=True)[:n]
+    return sorted_genres
+
 
 # HOME PAGE
 @app.route("/home")
@@ -63,24 +92,35 @@ def home():
     data_saved_tracks = requests.get(url_saved_tracks, headers=header).json()
     data_recently_played = requests.get(url_recently_played, headers=header).json()
     data_current_user = requests.get(url_current_user, headers=header).json()
-    
+    data_top_genres = requests.get(url_top_genres, headers=header).json()
+
+
+    # Get the top 10 genres by occurrences
+    genre_counts = get_genre_counts(data_top_genres)
+    top_10_genres = get_top_n_genres(genre_counts)
+
     top1_artist_id = data_top_artists['items'][0]['id']
-    url_recommandations = f'https://api.spotify.com/v1/me/following?type=artist&after={top1_artist_id}&limit=10'
-    data_recommandations = requests.get(url_recommandations, headers=header).json()
+    top1_genre = data_top_genres['items'][0]['genres'][0]
+    top1_genre_result = top1_genre.replace(' ', '+')
+
+    url_recommendations = f'https://api.spotify.com/v1/recommendations?seed_artists={top1_artist_id}&seed_genres={top1_genre_result}&seed_tracks={top1_artist_id}'
+    data_recommendations = requests.get(url_recommendations, headers=header).json()
 
     return render_template(
         "home.html",
-        data_top_artists=data_top_artists, 
+        data_top_artists=data_top_artists,
         data_top_tracks=data_top_tracks,
-        data_saved_tracks=data_saved_tracks, 
-        data_recently_played=data_recently_played, 
-        data_current_user=data_current_user, 
-        data_recommandations=data_recommandations
+        data_saved_tracks=data_saved_tracks,
+        data_recently_played=data_recently_played,
+        data_current_user=data_current_user,
+        top_10_genres=top_10_genres,
+        data_recommendations=data_recommendations
         )
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+    return 'Spotify was unable to gather all the necessary information, make sure you have listening time before going to this site', 500
+
 
 if __name__ == "__main__":
     app.run()
